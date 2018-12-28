3e9aab900d71cb670e6f3351d56511a8a9b2a4b3
Azmi Kamis <azmikamis@outlook.com>
2018-12-28 15:43:57 +0800

    Update and rename deploy_minikube_kubeflow.sh to setup_tools.sh

--- setup_tools.sh
+++ setup_tools.sh
@@ -1,9 +1,7 @@
 sudo apt update && sudo apt upgrade -y
 
 sudo apt install libvirt-bin qemu-kvm -y
-
 sudo usermod -aG libvirtd $(whoami)
-newgrp libvirtd
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
@@ -23,8 +21,8 @@ mkdir -p ${KUBEFLOW_SRC}
 cd ${KUBEFLOW_SRC}
 curl https://raw.githubusercontent.com/kubeflow/kubeflow/${KUBEFLOW_TAG}/scripts/download.sh | bash
 cd ~
-sudo ln -s ${KUBEFLOW_SRC}/scripts/kfctl.sh /usr/local/bin/kfctl
-sudo chown -h $(whoami):$(whoami) /usr/local/bin/kfctl
+sudo ln -s ${KUBEFLOW_SRC}/scripts/kfctl.sh ${KUBEFLOW_SRC}/scripts/kfctl
+sudo chown -h $(whoami):$(whoami) ${KUBEFLOW_SRC}/scripts/kfctl
 
 KS_VER=0.13.1
 KSFILE_VER=ks_${KS_VER}_linux_amd64
@@ -33,14 +31,3 @@ tar -xvf /tmp/${KSFILE_VER}.tar.gz -C /tmp
 sudo mv /tmp/${KSFILE_VER}/ks /usr/local/bin/
 rm -rf /tmp/${KSFILE_VER}
 rm /tmp/${KSFILE_VER}.tar.gz
-
-minikube config set WantReportErrorPrompt false
-minikube config set vm-driver kvm2
-minikube start --cpus 4 --memory 8096 --disk-size=40g
-minikube addons enable ingress
-
-KFAPP=kubeflow-lab
-kfctl init ${KFAPP} --platform minikube
-cd ${KFAPP}
-kfctl generate all
-kfctl apply all
